2cdc8cf2f8ec4133d932aad3cc9f57efb420cf0e
Krittin SETDHAVANICH <krittin.set@ku.th>
2025-12-30 20:44:13 +0700

    feat: added a code for yolo training

---
+++ src/tracking_backend_logic/__init__.py
@@ -0,0 +1,23 @@
+"""
+This package adapts core tracking and Re-ID components from the SpotOn backend
+for use within the SpotOn ML MLOps framework.
+"""
+from .common_types_adapter import (
+    CameraID, TrackID, GlobalID, FeatureVector, BoundingBoxXYXY,
+    TrackKey, QuadrantName, ExitRuleModelAdapter, CameraHandoffDetailConfigAdapter,
+    HandoffTriggerInfo, QUADRANT_REGIONS_TEMPLATE
+)
+from .botsort_tracker_adapter import BotSortTrackerAdapter
+from .camera_tracker_factory_adapter import CameraTrackerFactoryAdapter
+from .reid_manager_adapter import ReIDManagerAdapter
+from .handoff_logic_adapter import check_handoff_triggers_for_camera_adapter
+
+__all__ = [
+    "CameraID", "TrackID", "GlobalID", "FeatureVector", "BoundingBoxXYXY",
+    "TrackKey", "QuadrantName", "ExitRuleModelAdapter", "CameraHandoffDetailConfigAdapter",
+    "HandoffTriggerInfo", "QUADRANT_REGIONS_TEMPLATE",
+    "BotSortTrackerAdapter",
+    "CameraTrackerFactoryAdapter",
+    "ReIDManagerAdapter",
+    "check_handoff_triggers_for_camera_adapter"
+]
